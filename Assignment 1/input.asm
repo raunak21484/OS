66c8daec7686cc9742d7c736e1ab66c08d36bf3e
raunak114 <thakurraunak114@gmail.com>
2022-09-20 20:06:52 +0530

    Ass1

---
+++ Assignment 1/input.asm
@@ -0,0 +1,53 @@
+global main
+extern printf, scanf
+
+
+section .data
+    msgs: db "Enter a string: ",0
+    msgi: db "Enter a number (Integer): ",0
+    integ: db "%d",0
+    strng: db "%s",0
+    stringvar times 100 db 0
+
+
+section .text
+main:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 16
+
+
+    xor eax,eax
+    lea rdi, [msgi]
+    call printf
+
+
+    mov eax, 0
+    lea rdi, [integ]
+    lea rsi, [num]
+    call scanf
+
+    xor eax, eax
+    lea rdi, [num] 
+    call printf
+
+    xor eax,eax
+    lea rdi, [msgs]
+    call printf
+
+    mov eax, 0
+    lea rdi, [strng]
+    lea rsi, [stringvar]
+    call scanf
+
+    xor eax, eax
+    lea rdi, [stringvar]
+    call printf
+
+    add rsp, 16
+    leave
+    ret
+
+
+section .bss
+    num resb 4
