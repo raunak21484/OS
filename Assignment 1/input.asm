7a38f7c6f0bc041cf45681427db1c6eaf7121bdf
raunak114 <thakurraunak114@gmail.com>
2022-09-20 20:26:09 +0530

    ASS1v2

--- Assignment 1/input.asm
+++ Assignment 1/input.asm
@@ -5,6 +5,9 @@ extern printf, scanf
 section .data
     msgs: db "Enter a string: ",0
     msgi: db "Enter a number (Integer): ",0
+    msgos: db "The string you have entered is: "
+    msgoi: db "The number you have entered is: "
+    msgnl: db "",0
     integ: db "%d",0
     strng: db "%s",0
     stringvar times 100 db 0
@@ -27,9 +30,7 @@ main:
     lea rsi, [num]
     call scanf
 
-    xor eax, eax
-    lea rdi, [num] 
-    call printf
+    
 
     xor eax,eax
     lea rdi, [msgs]
@@ -41,7 +42,27 @@ main:
     call scanf
 
     xor eax, eax
+    lea rdi, [msgoi] 
+    call printf
+
+    xor eax, eax
+    lea rdi, [num] 
+    call printf
+
+    xor eax, eax
+    lea rdi, [msgnl] 
+    call printf
+
+    xor eax, eax
+    lea rdi, [msgos]
+    call printf
+    
+    xor eax, eax
     lea rdi, [stringvar]
+    call printf
+
+    xor eax, eax
+    lea rdi, [msgnl] 
     call printf
 
     add rsp, 16
