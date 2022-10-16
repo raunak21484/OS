1400ccad6e002a4315ce72077e68b0fc1289f1ba
raunak114 <thakurraunak114@gmail.com>
2022-10-16 20:27:21 +0530

    vv

--- padhai/Ostep/Chapter 5/fig5_2.c
+++ padhai/Ostep/Chapter 5/fig5_2.c
@@ -7,7 +7,7 @@ int
 main(int argc, char *argv[])
 {
 printf("hello world (pid:%d)\n", (int) getpid());
-
+int x  = 3;
 int rc = fork();
 printf("RC: %d\n");
 if (rc < 0) {
@@ -15,11 +15,17 @@ if (rc < 0) {
 fprintf(stderr, "fork failed\n");;
 exit(1);;
 } else if (rc == 0) { // child (new process)
+printf("c1: x = %d",x);
+x++;
+printf("c2: x = %d",x);
 printf("hello, I am child (pid:%d)\n", (int) getpid());;
 } else {
+    x++;
+    printf("p1: x = %d",x);
 // parent goes down this path (main);
     int wc = wait(NULL);
-printf("hello, I am parent of %d, (wd:%d) (pid:%d)\n",rc,wc, (int) getpid());
+    x++;
+printf("hello, I am parent of %d, (wd:%d) (pid:%d)\n x = %d",rc,wc, (int) getpid(),x);
 }
 return 0;
 }
--- padhai/Ostep/Chapter 5/problemstatement1.c
+++ padhai/Ostep/Chapter 5/problemstatement1.c
@@ -8,16 +8,16 @@ int primes[10] = {1,2,3,4,5,6,7,8,9,10};
 int i = 0;
 pthread_mutex_t lock;
 void* printPrime(){
-    pthread_mutex_lock(lock);
+    pthread_mutex_lock(&lock);
     printf("%d <--- \n",primes[i]);
     i++;
-    pthread_mutex_unlock(lock);
+    pthread_mutex_unlock(&lock);
 }
 int main(int argc, char *argv[]){
     pthread_t threads[10];
     pthread_mutex_init(&lock,NULL);
     for(int i=0;i<10;i++){
-        pthread_create(&(threads[i]),NULL,NULL,NULL);
+        pthread_create(&(threads[i]),NULL,&printPrime,NULL);
     }
     for(int i=0;i<10;i++){
         pthread_join(threads[i], NULL);
