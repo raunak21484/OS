0412be84c735920dc94c3e3600b58a57f42c25ce
raunak114 <thakurraunak114@gmail.com>
2022-10-25 20:35:51 +0530

    test1

--- Assignment 2/a2.c
+++ Assignment 2/a2.c
@@ -7,14 +7,53 @@ char **getInput(){
     char *line = NULL;
     size_t length = 0;
     getline(&line,&length,stdin);
-    printf("Your input of size %d, is %s",length,line);
-    return NULL;
+    //printf("Your input of size %d, is %s",length,line);
+    int c= 0;
+    while(line[c]!='\0'){
+        if(line[c]=='\n'||line[c]==' '|| line[c] == '\t'){
+            c++;
+        }
+    }
+    int *sizes =  (int*)malloc(sizeof(int )*c);
+    for(int i=0;i<c;i++){
+        sizes[i] =0 ;
+    }
+    int ptr = 0,sp=0;
+    while(line[ptr]!='\0'){
+        if(line[ptr]=='\n' || line[ptr]==' ' || line[ptr] == '\t'){
+            sp++;
+            ptr++;
+            continue;
+        }
+        sizes[sp]++;
+        ptr++;
+
+    }
+    char** segment = (char**) malloc(sizeof(char*)*(c+1));
+    for(int i=0;i<c;i++){
+        segment[i] = (char*)malloc(sizeof(char) * sizes[i]);
+    }
+    int ptr = 0;
+    for(int i=0;i<c;i++){
+        for(int j=0;j<sizes[i];j++){
+            segment[i][j] = line[ptr];
+            ptr++;
+        }
+    }
+    segment[c+1] = (char*)malloc(sizeof(char*));
+    segment[c+1][0] = '\0';
+    return segment;
 }
 void shell_loop(){
 
 }
 int main(int argc, char* argv[]){
     shell_loop();
-    getInput();
+    char** segment = getInput();
+    int ptr =0;
+    while(segment[ptr][0]!='0'){
+        printf("%s\n",segment[ptr]);
+        ptr++;
+    }
 return 0;
 }
---
+++ ass1 test/test.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+    printf("test 123 \r test2");
+    return 0;
+}
