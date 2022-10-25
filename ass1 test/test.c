2c14488fa4bf3bf96e8956f542132ef9b1da14ed
raunak114 <thakurraunak114@gmail.com>
2022-10-26 00:38:09 +0530

    test12

--- ass1 test/test.c
+++ ass1 test/test.c
@@ -5,9 +5,9 @@ int main(){
     int *p = (int*)malloc(sizeof(int)*100);
     printf("location of p: %p",p);
     free(p);
-    int *p = (int*)malloc(sizeof(int)*100);
+    p = (int*)malloc(sizeof(int)*100);
     printf("location of p: %p",p);
-    int *p = (int*)malloc(sizeof(int)*100);
+    p = (int*)malloc(sizeof(int)*100);
     printf("location of p: %p",p);
     return 0;
 }
