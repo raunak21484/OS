fe961d21ba356b1c1b58e839278dec9b7e68c0d6
raunak114 <thakurraunak114@gmail.com>
2022-10-26 15:55:00 +0530

    test12

--- Assignment 2/a2.c
+++ Assignment 2/a2.c
@@ -84,11 +84,11 @@ void changeDir(char ** segment){
         ptr1++;
         c++;
     }
-    if(ptr1==1 || strcmp(segment[1]," ")==0){
+    if(ptr1==1){
         chdir("/home");
         return;
     }
-
+    printf("segment[1] is '%s'\n",segment[1]);
     char* newDir = (char*)malloc((c+1)*sizeof(char));
     newDir[c] = '\0';
     ptr1 = 1;
