8d0c32c289eb40d131079523b2d1f0fb985e2a4f
raunak114 <thakurraunak114@gmail.com>
2022-10-26 17:35:32 +0530

    test12

--- ass1 test/testmain.c
+++ ass1 test/testmain.c
@@ -9,19 +9,13 @@ int main(int argc, char* argv[]){
     int test = fork();
     if(test<0){printf("FAILED!");}
     if(test==0){
-        int fork2= fork();
-        if(fork2<0){printf("FORK FAILED!");}
-        else if(fork2==0){
-        char* argv[5] = {"gcc","-o","test.o","test.c",NULL};
-        execvp("gcc",argv);
-
-        printf("FORK 2 FAILED!");
+        char *argv2[2] = {"./test.o", NULL};
+        execv("./test.o", argv2);
+        printf("FORK 1 FAILED!");
         }
         else {
             wait(NULL);
-            char *argv2[2] = {"./test.o", NULL};
-            execv("./test.o", argv2);
-            printf("FORK 1 FAILED!");
+
         }
     }else{
         wait(NULL);
