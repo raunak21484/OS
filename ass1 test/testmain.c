02ec0d4daccbd99f8a88972f0f3f68ec5dc1c3e2
raunak114 <thakurraunak114@gmail.com>
2022-10-26 17:05:41 +0530

    test12

---
+++ ass1 test/testmain.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<libgen.h>
+
+int main(int argc, char* argv[]){
+    int test = fork();
+    if(test<0){printf("FAILED!");}
+    if(test==0){
+        execve("./test.c",NULL,NULL);
+        printf("MAA");
+    }else{
+        wait(NULL);
+    }
+}
