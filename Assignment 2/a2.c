8d3c6932dfcba68ff8c5fb2299c84705255e86e4
raunak114 <thakurraunak114@gmail.com>
2022-10-26 15:39:02 +0530

    test12

--- Assignment 2/a2.c
+++ Assignment 2/a2.c
@@ -109,6 +109,7 @@ void changeDir(char ** segment){
     if(fin!=0){
         printf("Could not find directory: %s",temp);
     }
+    free(temp);
     //printf("SUC\n");
 //    char * temp = delim(segment[1]);
 //    chdir(temp);
