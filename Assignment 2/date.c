4688ed2ffddb833a00f663f746e4fa5ad6e3f9f9
raunak114 <thakurraunak114@gmail.com>
2022-10-27 17:36:22 +0530

    test12

--- Assignment 2/date.c
+++ Assignment 2/date.c
@@ -5,9 +5,14 @@
 #include<sys/wait.h>
 #include<libgen.h>
 #include<sys/stat.h>
-int main(int argc, char* argv[]){
-    if((strcmp(argv[1]),"default")==0){
+#include<time.h>
 
+int main(int argc, char* argv[]){
+    time_t time = time(NULL);
+    struct tm currtime =(*localtime(&time));
+    char* bigarray[12] = {"January","February","March","April","May","June","July","August","September","October",""};
+    if((strcmp(argv[1]),"default")==0){
+        printf("")
     }
     return 0;
 }
