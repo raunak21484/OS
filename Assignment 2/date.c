a6c445013cac27faeef03b66ef61019d72f0c101
raunak114 <thakurraunak114@gmail.com>
2022-10-27 17:54:39 +0530

    test12

--- Assignment 2/date.c
+++ Assignment 2/date.c
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]){
     char* smallarrayM[13] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec",NULL};
     char* bigArrayD[8] = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",NULL};
     char* smallArrayD[8] = {"Sun","Mon","Tue","Wed","Thur","Fri","Sat",NULL};
-    if((strcmp(argv[1]),"default")==0){
+    if(strcmp(argv[1],"default")==0){
         printf("%s %d %s %d %d:%d:%d %s UTC",bigArrayD[currtime.tm_wday],currtime.tm_mday,bigarrayM[currtime.tm_mon],(currtime.tm_year+1900),((currtime.tm_hour>=12?currtime.tm_hour-12:currtime.tm_hour)),currtime.tm_min,currtime.tm_sec,((currtime.tm_hour>=12?"PM":"AM")));
         return 0;
     }
