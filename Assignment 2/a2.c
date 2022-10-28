8c0ab242a4b3d27f9d338e12c5814ee631b1c318
raunak114 <thakurraunak114@gmail.com>
2022-10-28 14:42:00 +0530

    test12

--- Assignment 2/a2.c
+++ Assignment 2/a2.c
@@ -216,12 +216,13 @@ void date(char ** segment){
         if(segment[1]==NULL || strcmp(segment[1],"-u")==0 || strcmp(segment[1],"--utc")==0 || strcmp(segment[1],"--universal")==0 ||
                                     strcmp(segment[1],"-u\n")==0 || strcmp(segment[1],"--utc\n")==0 || strcmp(segment[1],"--universal\n")==0||strcmp(segment[1],"\n")==0){
             //segment[0][strlen(segment[0])-1] = '\0';
-
+            printf("in HEREEE!!");
             char* argv[3]=  {mkname,"default",NULL};
             char* env[1] = {NULL};
             execve(mkname,argv,env);
         }else if(strcmp(segment[1],"-R")==0 || strcmp(segment[1],"--rfc-email")==0  || strcmp(segment[1],"-R\n")==0 || strcmp(segment[1],"--rfc-email\n")==0){
-                char *argv[3] = {mkname,"r",NULL};
+                printf("in here!!!");
+            char *argv[3] = {mkname,"r",NULL};
                 char* env[1] = {NULL};
                 execve(mkname,argv,env);
             }
