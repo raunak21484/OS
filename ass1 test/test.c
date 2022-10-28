9e5b5cbb301194b38fe4e3f5e54d4f1450ccb6a9
raunak114 <thakurraunak114@gmail.com>
2022-10-28 15:16:00 +0530

    test12

--- ass1 test/test.c
+++ ass1 test/test.c
@@ -5,18 +5,22 @@
 #include<sys/wait.h>
 #include<libgen.h>
 char * concatString(char * string1, char * string2){
-    int c = strlen(string1)+strlen(string2);
+    int c1 = strlen(string1);
+    int c2 = strlen(string2);
+    int c = c1+c2;
     printf("c = %d",c);
     char* stringfinal = (char*)malloc(sizeof(char)*(c+1));
-    for(int i=0;i< strlen(string1);i++){
+
+    for(int i=0;i< c1;i++){
         stringfinal[i] = string1[i];
     }
-    int c1 = strlen(string1);
-    for(int i=0;i< strlen(string2);i++){
+
+    for(int i=0;i< c2;i++){
         stringfinal[i+ c1] = string2[i];
     }
     stringfinal[c] = '\0';
-    printf("stringfinal = %s",stringfinal);
+    printf("here");
+    //printf("stringfinal = %s",stringfinal);
     return stringfinal;
 }
 int main()
@@ -25,6 +29,6 @@ int main()
     char* bi;
     strcpy(bi,test);
    char * third = concatString(test,bi);
-   printf("third = %s\n",third);
+  // printf("third = %s\n",third);
     return 0;
 }
