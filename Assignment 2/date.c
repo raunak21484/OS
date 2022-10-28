8cac3a7832cde6b5658ce7b8c1440eae9a1027bf
raunak114 <thakurraunak114@gmail.com>
2022-10-28 14:29:08 +0530

    test12

--- Assignment 2/date.c
+++ Assignment 2/date.c
@@ -12,13 +12,13 @@ int main(int argc, char* argv[]){
     struct tm currtime =(*localtime(&time1));
     char* bigarrayM[13] = {"January","February","March","April","May","June","July","August","September","October","November","December",NULL};
     char* smallarrayM[13] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec",NULL};
-    char* bigArrayD[8] = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",NULL};
+    char* bigArrayD[8] = {"Sund ay","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",NULL};
     char* smallArrayD[8] = {"Sun","Mon","Tue","Wed","Thur","Fri","Sat",NULL};
     if(strcmp(argv[1],"default")==0){
         printf("%s %d %s %d %d:%d:%d %s UTC\n",bigArrayD[currtime.tm_wday],currtime.tm_mday,bigarrayM[currtime.tm_mon],(currtime.tm_year+1900),((currtime.tm_hour>=12?currtime.tm_hour-12:currtime.tm_hour)),currtime.tm_min,currtime.tm_sec,((currtime.tm_hour>=12?"PM":"AM")));
         return 0;
-    }else{
-
+    }else if(strcmp(argv[1],"r")){
+        printf("%s, %d %s %d %d:%d:%d +0000",smallArrayD[currtime.tm_wday],currtime.tm_mday,smallarrayM[currtime.tm_mon],(currtime.tm_year+1900),currtime.tm_hour>=12,currtime.tm_min,currtime.tm_sec);
     }
-    return 0;
+    return 1;
 }
