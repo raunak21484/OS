1175af401ef748ff35761aeb26f1ab8c29d35107
raunak114 <thakurraunak114@gmail.com>
2022-10-30 15:52:26 +0530

    test12

--- Assignment 2/ls.c
+++ Assignment 2/ls.c
@@ -8,5 +8,16 @@
 
 int main(int argc, char* argv[]){//path, "XX"->"-a and -m flag", "total path", NULL
     printf("%s\n%s\n%s\n",argv[0],argv[1],argv[2]);
+    char[5] sep;
+    if(argv[1][1] == '1'){
+        sep = ", ";
+    }else{
+        sep = " ";
+    }
+    DIR *directory;
+    directory = opendir(argv[2]);
+    struct dirent *dir;
+    int dispdot = (int)(argv[1][0]-'0');
+
     return 0;
 }
