3d5d6f54c07eb5f12a9f69cd045a0491335f128b
raunak114 <thakurraunak114@gmail.com>
2022-10-31 02:31:37 +0530

    test12

--- Assignment 2/a2.c
+++ Assignment 2/a2.c
@@ -619,7 +619,8 @@ void cat(char ** segment){
         }
     }
 }
-void* syscall(char** args){
+void* syscall(void* segment){
+    char** args = *(char**)segment;
     int c =0;
     for(int i=0;args[i]!=NULL;i++){
         c+=strlen(args[i]);
