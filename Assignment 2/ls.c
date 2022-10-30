505370f82b066fd95e2a057bdab4d6d6f8c794cb
raunak114 <thakurraunak114@gmail.com>
2022-10-30 20:40:13 +0530

    test12

--- Assignment 2/ls.c
+++ Assignment 2/ls.c
@@ -23,7 +23,7 @@ int main(int argc, char* argv[]){//path, "XX"->"-a and -m flag", "total path", N
                 if(dispdot){
                     i++;
                 if(i%2){printf("\033[0;33m%s\033[0m",filetemp->d_name);}
-                else{printf("\033[0;32mm%s\033[0m",filetemp->d_name);}
+                else{printf("\033[0;32m%s\033[0m",filetemp->d_name);}
                 if(sepcomma&& filetemp2!=NULL){printf(",");}
                 printf(" ");
                 }
