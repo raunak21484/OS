fae7177eb320f55fb79e9ee3e0e916f5491dc3c8
raunak114 <thakurraunak114@gmail.com>
2022-10-30 21:18:53 +0530

    test12

--- Assignment 2/a2.c
+++ Assignment 2/a2.c
@@ -50,7 +50,7 @@ void echo(char** segment){
         marking = -1;dollarmarking=-1;
         while(segment[ptr][ptr2]!='\0'){
             if(segment[ptr][ptr2]=='\\'){
-                if(segment[ptr][ptr2+1]!=NULL && marking!= ptr2){
+                if(segment[ptr][ptr2+1]!='\0' && marking!= ptr2){
                     marking = ptr2+1;
                     printf("%c",segment[ptr][ptr2+1]);}
                 ptr2++;
@@ -92,7 +92,7 @@ char* echoMessage(char** segment, int ptr, char sep){
 
             if(segment[ptr][ptr2]=='\\'){
                 //printf("FOUND1!!");
-                if(segment[ptr][ptr2+1]!=NULL && marking!= ptr2){
+                if(segment[ptr][ptr2+1]!='\0' && marking!= ptr2){
                     marking = ptr2+1;
                     //printf("%c",'\\');
                     ECHOMESSAGE[eptr] = segment[ptr][ptr2+1];
