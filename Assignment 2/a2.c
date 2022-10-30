75090ba0c9ddf1e05203e42fdde2ad1bdaed8bc8
raunak114 <thakurraunak114@gmail.com>
2022-10-30 16:14:23 +0530

    test12

--- Assignment 2/a2.c
+++ Assignment 2/a2.c
@@ -401,7 +401,7 @@ void ls(char ** segment){
         else{
             int status;
             wait(&status);
-            if(status = 50){printf("Directory Does not exist!\n");}
+            if(status == 50){printf("Directory Does not exist!\n");}
             else if(status!=0){printf("Failed due to unexpected error! Code = %d |\n",status);}
         }
 
