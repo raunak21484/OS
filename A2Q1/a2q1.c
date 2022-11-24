daf69e8fdb75279d98cb71b3535e5dbf0ce6148d
raunak114 <thakurraunak114@gmail.com>
2022-11-24 19:15:08 +0530

    hgasd

--- A2Q1/a2q1.c
+++ A2Q1/a2q1.c
@@ -10,7 +10,7 @@
 
 
 
-void fcaller1(){
+void fcaller1(FILE *file){
     struct timespec s1,e1;
     int S1 = clock_gettime(CLOCK_REALTIME, &s1);
     pid_t pid1 = fork();
@@ -27,10 +27,11 @@ void fcaller1(){
         double ans=(e1.tv_sec-s1.tv_sec+(e1.tv_nsec-s1.tv_nsec)/(double)1e9);
         ;
         printf("----------RunTime for first thread: %lfs----------\n",(ans));
+        fprintf(file,"%lf\n",ans);
     }
 }
 
-void fcaller2(){
+void fcaller2(FILE *file){
     struct timespec s1,e1;
     int S1 = clock_gettime(CLOCK_REALTIME, &s1);
     pid_t pid2 = fork();
@@ -46,10 +47,11 @@ void fcaller2(){
         int E1=clock_gettime(CLOCK_REALTIME,&e1);
         double ans=(e1.tv_sec-s1.tv_sec+(e1.tv_nsec-s1.tv_nsec)/(double)1e9);
         printf("----------RunTime for second thread: %lfs----------\n",(ans));
+        fprintf(file,"%lf\n",ans);
     }
 }
 
-void fcaller3(){
+void fcaller3(FILE *file){
     struct timespec s1,e1;
     int S1 = clock_gettime(CLOCK_REALTIME, &s1);
     pid_t pid3 = fork();
@@ -65,34 +67,36 @@ void fcaller3(){
         int E1=clock_gettime(CLOCK_REALTIME,&e1);
         double ans=(e1.tv_sec-s1.tv_sec+(e1.tv_nsec-s1.tv_nsec)/(double)1e9);
         printf("----------RunTime for third thread: %lfs----------\n",(ans));
+        fprintf(file,"%lf\n",ans);
     }
 }
 
 
 int main(){
     pid_t pid1, pid2, pid3;
-
+    FILE *outputfile;
+    outputfile=fopen("Outputfile.txt","a");
     pid1 = fork();
     if(pid1<0){
         printf("Error pid1!\n");
         return 1;
     }
     if(pid1==0){
-        fcaller1();
+        fcaller1(outputfile);
         pid2 = fork();
         if(pid2<0){
             printf("Error pid2!\n");
             return 1;
         }
         if(pid2 ==0){
-            fcaller2();
+            fcaller2(outputfile);
             pid3 = fork();
             if(pid3<0){
                 printf("Error pid3!\n");
                 return 1;
             }
             if(pid3==0){
-                fcaller3();
+                fcaller3(outputfile);
             }else{
                 wait(NULL);
             }
@@ -102,5 +106,5 @@ int main(){
     }else{
         wait(NULL);
     }
-
+    fclose(outputfile);
 }
