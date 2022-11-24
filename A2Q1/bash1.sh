5fadf524788ea637b5e707f183879e16432652f9
raunak114 <thakurraunak114@gmail.com>
2022-11-24 19:07:57 +0530

    123

--- A2Q1/bash1.sh
+++ A2Q1/bash1.sh
@@ -1,2 +1,9 @@
-echo hello1!
-sleep 1s
+rm -r -f KernelBuild
+mkdir KernelBuild
+cp linux-5.19.9.tar.xz KernelBuild/linux-5.19.9.tar.xz
+cd KernelBuild
+tar -xvf linux-5.19.9.tar.xz
+cd linux-5.19.9
+make mrproper
+cp ../configFile ./.config
+make -j$(nproc)
